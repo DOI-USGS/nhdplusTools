653c0116ff186d5e504ede952cfda0883e7d10c4
David Blodgett <dblodgett@usgs.gov>
2018-07-05 15:10:07 -0500

    cleanup and rebuild pkgdown

--- R/refactor_nhdplus.R
+++ R/refactor_nhdplus.R
@@ -79,42 +79,51 @@ refactor_nhdplus <- function(nhdplus_flines,
   }
 
   if (three_pass) {
-    collapsed_flines <- collapse_flowlines(sf::st_set_geometry(flines, NULL),
-                                           (0.25 * collapse_flines_meters / 1000),
-                                           TRUE,
-                                           (0.25 * collapse_flines_main_meters / 1000))
+    collapsed_flines <-
+      collapse_flowlines(sf::st_set_geometry(flines, NULL),
+                         (0.25 * collapse_flines_meters / 1000),
+                         TRUE,
+                         (0.25 * collapse_flines_main_meters / 1000))
 
-    collapsed_flines <- collapse_flowlines(collapsed_flines,
-                                           (0.5 * collapse_flines_meters / 1000),
-                                           TRUE,
-                                           (0.5 * collapse_flines_main_meters / 1000),
-                                           warn = FALSE)
+    collapsed_flines <-
+      collapse_flowlines(collapsed_flines,
+                         (0.5 * collapse_flines_meters / 1000),
+                         TRUE,
+                         (0.5 * collapse_flines_main_meters / 1000),
+                         warn = FALSE)
 
-    collapsed_flines <- collapse_flowlines(collapsed_flines,
-                                           (collapse_flines_meters / 1000),
-                                           TRUE,
-                                           (collapse_flines_main_meters / 1000),
-                                           warn = FALSE)
+    collapsed_flines <-
+      collapse_flowlines(collapsed_flines,
+                         (collapse_flines_meters / 1000),
+                         TRUE,
+                         (collapse_flines_main_meters / 1000),
+                         warn = FALSE)
   } else {
-    collapsed_flines <- collapse_flowlines(sf::st_set_geometry(flines, NULL),
-                                           (collapse_flines_meters / 1000),
-                                           TRUE,
-                                           (collapse_flines_main_meters / 1000))
+    collapsed_flines <-
+      collapse_flowlines(sf::st_set_geometry(flines, NULL),
+                         (collapse_flines_meters / 1000),
+                         TRUE,
+                         (collapse_flines_main_meters / 1000))
   }
 
   collapsed_flines %>%
     dplyr::inner_join(select(flines, COMID), by = "COMID") %>%
     sf::st_as_sf() %>%
     sf::st_transform(4326) %>%
-    sf::st_write(out_collapsed, layer_options = "OVERWRITE=YES", quiet = !warn)
+    sf::st_write(out_collapsed, layer_options = "OVERWRITE=YES",
+                 quiet = !warn)
 
   if (warn) {
     message("collapse complete, out collapse written to disk, reconciling")
   }
 
-  collapsed <- reconcile_collapsed_flowlines(collapsed_flines, select(flines, COMID), id = "COMID")
+  collapsed <- reconcile_collapsed_flowlines(collapsed_flines,
+                                             select(flines, COMID),
+                                             id = "COMID")
 
-  collapsed$member_COMID <- unlist(lapply(collapsed$member_COMID, function(x) paste(x, collapse = ",")))
+  collapsed$member_COMID <-
+    unlist(lapply(collapsed$member_COMID,
+                  function(x) paste(x, collapse = ",")))
 
   sf::st_write(sf::st_transform(collapsed, 4326),
                out_reconciled,
@@ -125,12 +134,15 @@ refactor_nhdplus <- function(nhdplus_flines,
 
 #' @title Prep NHDPlus Data for Refactor
 #' @description Function to prep NHDPlus data for network refactoring
-#' @param flines data.frame NHDPlus flowlines including at a minimum: COMID, LENGTHKM,
-#' FTYPE, TerminalFl, FromNode, ToNode, TotDASqKM, StartFlag, StreamOrde,
-#' StreamCalc, TerminalPa, and Pathlength variables.
-#' @param min_network_size numeric Minimum size (sqkm) of drainage network to include in output.
-#' @param  min_path_length numeric Minimum length (km) of terminal level path of a network.
-#' @param purge_non_dendritic boolean Should non dendritic paths be removed or not.
+#' @param flines data.frame NHDPlus flowlines including at a minimum:
+#' COMID, LENGTHKM, FTYPE, TerminalFl, FromNode, ToNode, TotDASqKM,
+#' StartFlag, StreamOrde, StreamCalc, TerminalPa, and Pathlength variables.
+#' @param min_network_size numeric Minimum size (sqkm) of drainage network
+#' to include in output.
+#' @param  min_path_length numeric Minimum length (km) of terminal level
+#' path of a network.
+#' @param purge_non_dendritic boolean Should non dendritic paths be removed
+#' or not.
 #' @param warn boolean controls whether warning an status messages are printed
 #' @return data.frame ready to be used with the refactor_flowlines function.
 #' @importFrom dplyr select filter left_join
@@ -155,31 +167,39 @@ prepare_nhdplus <- function(flines,
                    StreamOrde, StreamCalc, TerminalPa, Pathlength, Divergence)
 
   if (purge_non_dendritic) {
-    flines <- filter(flines, FTYPE != "Coastline" & # Remove Coastlines
-                       StreamOrde == StreamCalc) #& # Also use streamorder and streamcalc to select only the main paths.
+    flines <- filter(flines, FTYPE != "Coastline" &
+                       StreamOrde == StreamCalc)
   } else {
-    flines <- filter(flines, FTYPE != "Coastline") # Remove Coastlines
+    flines <- filter(flines, FTYPE != "Coastline")
     flines[["FromNode"]][which(flines$Divergence == 2)] <- NA
   }
-  terminal_filter <- flines$TerminalFl == 1 & flines$TotDASqKM < min_network_size
-  start_filter <- flines$StartFlag == 1 & flines$Pathlength < min_path_length
+  terminal_filter <- flines$TerminalFl == 1 &
+    flines$TotDASqKM < min_network_size
+  start_filter <- flines$StartFlag == 1 &
+    flines$Pathlength < min_path_length
 
   if (any(terminal_filter) | any(start_filter)) {
 
     tiny_networks <- rbind(filter(flines, terminal_filter),
                            filter(flines, start_filter))
 
-    flines <- filter(flines, !flines$TerminalPa %in% unique(tiny_networks$TerminalPa))
+    flines <- filter(flines, !flines$TerminalPa %in%
+                       unique(tiny_networks$TerminalPa))
   }
   if (warn) {
-    warning(paste("Removed", orig_rows - nrow(flines), "flowlines that don't apply.\n",
+    warning(paste("Removed", orig_rows - nrow(flines),
+                  "flowlines that don't apply.\n",
                   "Includes: Coastlines, non-dendritic paths, \nand networks",
                   "with drainage area less than",
                   min_network_size, "sqkm"))
   }
 
-  # Join ToNode and FromNode along with COMID and Length to get downstream attributes.
-  flines <- left_join(flines, select(flines, toCOMID = COMID, FromNode), by = c("ToNode" = "FromNode"))
+  # Join ToNode and FromNode along with COMID and Length to
+  # get downstream attributes.
+  flines <- left_join(flines, select(flines,
+                                     toCOMID = COMID,
+                                     FromNode),
+                      by = c("ToNode" = "FromNode"))
 
   if (!all(flines[["TerminalFl"]][which(is.na(flines$toCOMID))] == 1)) {
     stop(paste("FromNode - ToNode imply terminal flowlines that are not\n",
@@ -187,5 +207,6 @@ prepare_nhdplus <- function(flines,
   }
 
   select(flines, -ToNode, -FromNode, -TerminalFl, -StartFlag,
-         -StreamOrde, -StreamCalc, -TerminalPa, -FTYPE, -Pathlength, -Divergence)
+         -StreamOrde, -StreamCalc, -TerminalPa,
+         -FTYPE, -Pathlength, -Divergence)
 }

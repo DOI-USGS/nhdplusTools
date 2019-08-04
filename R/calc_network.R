55315ac3a2dff28f9af287ab5452b5b7fd4b413b
David Blodgett <dblodgett@usgs.gov>
2019-08-04 14:44:54 -0500

    test coverage

--- R/calc_network.R
+++ R/calc_network.R
@@ -1,36 +1,3 @@
-get_dsLENGTHKM <- function(flines) {
-  # This gets all the next-downstream flowlines and finds the
-  # length of the next downstream
-  flines$dsLENGTHKM <-
-    flines[["LENGTHKM"]][match(flines$toCOMID, flines$COMID)]
-  # already removed comids get NA dsLength -- ok to set them to 0.
-  flines[["dsLENGTHKM"]][is.na(flines$dsLENGTHKM)] <- 0
-  flines[["dsLENGTHKM"]]
-}
-
-get_upstream <- function(flines) {
-  left_join(select(flines, COMID), select(flines, fromCOMID = COMID, toCOMID),
-            by = c("COMID" = "toCOMID"))
-}
-
-get_num_upstream <- function(flines) {
-  left_join(select(flines, COMID, toCOMID),
-            get_upstream(flines) %>%
-              group_by(COMID) %>%
-              summarise(num_upstream = n()),
-            by = "COMID")[["num_upstream"]]
-}
-
-get_ds_num_upstream <- function(flines) {
-  flines <- mutate(flines, num_upstream = get_num_upstream(flines))
-  flines[["num_upstream"]][match(flines$toCOMID, flines$COMID)]
-}
-
-get_ds_joined_fromCOMID <- function(flines) {
-  flines <- mutate(flines, ds_joined_fromCOMID = joined_fromCOMID)
-  flines[["ds_joined_fromCOMID"]][match(flines$toCOMID, flines$COMID)]
-}
-
 #' Total Drainage Area
 #' @description Calculates total drainage area given a dendritic
 #' network and incremental areas.

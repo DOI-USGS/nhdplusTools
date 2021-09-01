fc90749303b0f581d927bc2ece3d0710e85e417e
David Blodgett <dblodgett@usgs.gov>
2021-09-01 13:50:51 -0500

    rename_geometry utility function

--- R/nhdplusTools.R
+++ R/nhdplusTools.R
@@ -362,13 +362,32 @@ drop_geometry <- function(x) {
 #' st_compatibalize(one, two)
 #'
 st_compatibalize <- function(sf1, sf2) {
+
   sf1 <- st_transform(sf1, st_crs(sf2))
 
-  g <- attr(sf1, "sf_column")
-  gp <- attr(sf2, "sf_column")
-  names(sf1)[names(sf1) == g] <- gp
-  attr(sf1, "sf_column") <- gp
-  sf1
+  rename_geometry(sf1, attr(sf2, "sf_column"))
+
+}
+
+#' rename_geometry
+#' @description correctly renames the geometry column
+#' of a sf object.
+#' @param g sf data.table
+#' @param name character name to be used for geometry
+#' @export
+#' @example
+#'
+#' (g <- sf::st_sf(a=3, geo = sf::st_sfc(sf::st_point(1:2))))
+#' rename_geometry(g, "geometry")
+#'
+rename_geometry <- function(g, name){
+  current = attr(g, "sf_column")
+
+  names(g)[names(g)==current] = name
+
+  attr(g, "sf_column") <- name
+
+  g
 }
 
 get_cl <- function(cl) {

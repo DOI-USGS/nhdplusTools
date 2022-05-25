4848d458ca9d7541f2ffb36fe815de0c1e4c2533
David Blodgett <dblodgett@usgs.gov>
2022-05-25 08:27:54 -0500

    fix fst file name

--- R/nhdplusTools.R
+++ R/nhdplusTools.R
@@ -3,7 +3,7 @@ vaa_hydroshare <-
   'https://www.hydroshare.org/resource/6092c8a62fac45be97a09bfd0b0bf726/data/contents/nhdplusVAA.fst'
 
 vaa_sciencebase <-
-  'https://www.sciencebase.gov/catalog/file/get/60c92503d34e86b9389df1c9?f=__disk__eb%2Fe0%2F3f%2Febe03f6e23c5b37a854e50c4ae7875dbb846c143'
+  'https://www.sciencebase.gov/catalog/file/get/60c92503d34e86b9389df1c9?name=enhd_nhdplusatts.fst'
 
 nhdplusTools_env <- new.env()
 
